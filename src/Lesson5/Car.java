20f5548e23bb5eed3167e4584b439350359563e5
KatySid <whitekaty666@gmail.com>
2021-02-17 11:16:40 +0400

    lesson 5

--- src/Lesson5/Car.java
+++ src/Lesson5/Car.java
@@ -1,6 +1,8 @@
 package Lesson5;
 
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.FutureTask;
 
 public class Car implements Runnable {
@@ -11,15 +13,17 @@ public class Car implements Runnable {
         private Race race;
         private int speed;
         private String name;
+        private CyclicBarrier preparing;
         public String getName() {
             return name;
         }
         public int getSpeed() {
             return speed;
         }
-        public Car(Race race, int speed) {
+        public Car(Race race, int speed, CyclicBarrier preparing) {
             this.race = race;
             this.speed = speed;
+            this.preparing = preparing;
             CARS_COUNT++;
             this.name = "Участник #" + CARS_COUNT;
         }
@@ -30,11 +34,21 @@ public class Car implements Runnable {
                 System.out.println(this.name + " готовится");
                 Thread.sleep(500 + (int)(Math.random() * 800));
                 System.out.println(this.name + " готов");
+                preparing.await();
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
             for (int i = 0; i < race.getStages().size(); i++) {
                 race.getStages().get(i).go(this);
             }
+                try {
+                    preparing.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+
         }
     }
--- src/Lesson5/MainClass.java
+++ src/Lesson5/MainClass.java
@@ -1,18 +1,48 @@
 package Lesson5;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MainClass {
         public static final int CARS_COUNT = 4;
+        public static  CyclicBarrier preparing = new CyclicBarrier(CARS_COUNT+1);
         public static void main(String[] args) {
             System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
             Race race = new Race(new Road(60), new Tunnel(), new Road(40));
             Car[] cars = new Car[CARS_COUNT];
             for (int i = 0; i < cars.length; i++) {
-                cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+                cars[i] = new Car(race, 20 + (int) (Math.random() * 10), preparing);
             }
-            for (int i = 0; i < cars.length; i++) {
-                new Thread(cars[i]).start();
+            ExecutorService raceCar = Executors.newFixedThreadPool(CARS_COUNT);
+            for (int i = 0; i < cars.length; i++){
+                int finalI = i;
+                raceCar.execute(()->{
+                    cars[finalI].run();
+                });
+            };
+
+
+            try {
+                preparing.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
             }
             System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+
+
+            try {
+                preparing.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+
             System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+            raceCar.shutdown();
         }
     }
